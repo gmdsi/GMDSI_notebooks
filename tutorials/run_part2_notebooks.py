1345dc258d8174946998cd6d0064dc76404df86f
jdub <JWhite@localadmins-MacBook-Pro.local>
2022-04-20 20:22:56 -0600

    added clear option

--- tutorials/run_part2_notebooks.py
+++ tutorials/run_part2_notebooks.py
@@ -1,27 +1,40 @@
 import os
 cwd = os.getcwd()
 
+clear = False
+
 os.chdir("part2_pstfrom_pest_setup")
 os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --allow-errors --inplace freyberg_pstfrom_pest_setup.ipynb")
 os.system("jupyter nbconvert --to pdf freyberg_pstfrom_pest_setup.ipynb")
+if clear:
+    os.system("jupyter nbconvert --ClearOutputPreprocessor.enabled=True --allow-errors --inplace freyberg_pstfrom_pest_setup.ipynb.ipynb")
+
 os.chdir(cwd)
 
 os.chdir("part2_obs_and_weights")
 os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --allow-errors --inplace freyberg_obs_and_weights.ipynb")
 os.system("jupyter nbconvert --to pdf freyberg_obs_and_weights.ipynb")
+if clear:
+    os.system("jupyter nbconvert --ClearOutputPreprocessor.enabled=True --allow-errors --inplace freyberg_obs_and_weights.ipynb")
 os.chdir(cwd)
 
 os.chdir("part2_ies")
 os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --allow-errors --inplace freyberg_ies.ipynb")
 os.system("jupyter nbconvert --to pdf freyberg_ies.ipynb")
+if clear:
+    os.system("jupyter nbconvert --ClearOutputPreprocessor.enabled=True --allow-errors --inplace freyberg_ies.ipynb")
 os.chdir(cwd)
 
 os.chdir("part2_da")
 os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --allow-errors --inplace freyberg_da_prep.ipynb")
 os.system("jupyter nbconvert --to pdf freyberg_da_prep.ipynb")
+if clear:
+    os.system("jupyter nbconvert --ClearOutputPreprocessor.enabled=True --allow-errors --inplace freyberg_da_prep.ipynb")
 os.chdir(cwd)
 
 os.chdir("part2_da")
 os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --allow-errors --inplace freyberg_da_run.ipynb")
 os.system("jupyter nbconvert --to pdf freyberg_da_run.ipynb")
+if clear:
+    os.system("jupyter nbconvert --ClearOutputPreprocessor.enabled=True --allow-errors --inplace freyberg_da_run.ipynb")
 os.chdir(cwd)
