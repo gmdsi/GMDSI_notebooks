b3d7454fcf6b28d9190e5355f0eee03729dcb267
Rui Hugman <hugm0001@flinders.edu.au>
2022-04-27 16:15:15 +0930

    Update run_part2_notebooks.py

--- tutorials/run_part2_notebooks.py
+++ tutorials/run_part2_notebooks.py
@@ -5,16 +5,16 @@ cwd = os.getcwd()
 clear = False
 pdf = False
 
-os.chdir("part2_pstfrom_pest_setup")
+os.chdir("part2_1_pstfrom_pest_setup")
 os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --allow-errors --inplace freyberg_pstfrom_pest_setup.ipynb")
 if pdf:
     os.system("jupyter nbconvert --to pdf freyberg_pstfrom_pest_setup.ipynb")
 if clear:
-    os.system("jupyter nbconvert --ClearOutputPreprocessor.enabled=True --ClearMetadataPreprocessor.enabled=True --allow-errors --inplace freyberg_pstfrom_pest_setup.ipynb.ipynb")
+    os.system("jupyter nbconvert --ClearOutputPreprocessor.enabled=True --ClearMetadataPreprocessor.enabled=True --allow-errors --inplace freyberg_pstfrom_pest_setup.ipynb")
 
 os.chdir(cwd)
 
-os.chdir("part2_obs_and_weights")
+os.chdir("part2_2_obs_and_weights")
 os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --allow-errors --inplace freyberg_obs_and_weights.ipynb")
 if pdf:
     os.system("jupyter nbconvert --to pdf freyberg_obs_and_weights.ipynb")
@@ -22,7 +22,7 @@ if clear:
     os.system("jupyter nbconvert --ClearOutputPreprocessor.enabled=True --ClearMetadataPreprocessor.enabled=True --allow-errors --inplace freyberg_obs_and_weights.ipynb")
 os.chdir(cwd)
 
-os.chdir("part2_glm")
+os.chdir("part2_4_glm")
 # glm1
 os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=180000 --allow-errors --inplace freyberg_glm_1.ipynb")
 if pdf:
@@ -37,7 +37,7 @@ if clear:
     os.system("jupyter nbconvert --ClearOutputPreprocessor.enabled=True --ClearMetadataPreprocessor.enabled=True --allow-errors --inplace freyberg_glm_2.ipynb")
 os.chdir(cwd)
 
-os.chdir("part2_ies")
+os.chdir("part2_6_ies")
 os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --allow-errors --inplace freyberg_ies.ipynb")
 if pdf:
     os.system("jupyter nbconvert --to pdf freyberg_ies.ipynb")
