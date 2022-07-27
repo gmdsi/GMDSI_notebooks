1c6a02b3499b39ee54b7ae6ad57968594bd4fd9e
rhugman <60137311+hydroanalytica@users.noreply.github.com>
2022-07-27 16:08:02 +0930

    renaming dirs

---
+++ tutorials/part1_10_pilotpoints_run/forward_run.py
@@ -0,0 +1,7 @@
+import os
+import shutil
+import pandas as pd
+import numpy as np
+import pyemu
+import flopy
+pp_file = 'hkpp.dat'hk_arr = pyemu.geostats.fac2real(pp_file, factors_file=pp_file+'.fac',out_file=None)pyemu.os_utils.run('mf6')pyemu.os_utils.run('mp7 freyberg_mp.mpsim')
---
+++ tutorials/part1_7_intro_to_bayes/bayes_helper.py
@@ -0,0 +1,48 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+sys.path.append('..')
+import pyemu
+import matplotlib as mpl
+
+#--modify default matplotlib settings
+mpl.rcParams['font.sans-serif']          = 'Univers 57 Condensed'
+mpl.rcParams['font.serif']               = 'Times'
+mpl.rcParams['pdf.compression']          = 0
+mpl.rcParams['pdf.fonttype']             = 42
+#--figure text sizes
+mpl.rcParams['legend.fontsize']  = 12
+mpl.rcParams['axes.labelsize']   = 12
+mpl.rcParams['xtick.labelsize']  = 12
+mpl.rcParams['ytick.labelsize']  = 12
+
+def gaussian_multiply(mu1,std1,mu2,std2):
+    var1,var2 = std1**2,std2**2
+    mean = (var1*mu2 + var2*mu1) / (var1 + var2)
+    variance = (var1 * var2) / (var1 + var2)
+    return mean, np.sqrt(variance)
+
+
+def plot_posterior(prior_mean, prior_std, likeli_mean, likeli_std, legend=True, savefigure=False):
+    plt.figure()
+
+    post_mean, post_std = gaussian_multiply(prior_mean, prior_std, likeli_mean, likeli_std)
+
+    xs, ys = pyemu.plot.plot_utils.gaussian_distribution(prior_mean, prior_std)
+    plt.plot(xs, ys, color='k', ls='--', lw=3.0, label='prior')
+
+    xs, ys = pyemu.plot.plot_utils.gaussian_distribution(likeli_mean, likeli_std)
+    plt.plot(xs, ys, color='g', ls='--', lw=3.0, label='likelihood')
+
+    xs, ys = pyemu.plot.plot_utils.gaussian_distribution(post_mean, post_std)
+    plt.fill_between(xs, 0, ys, label='posterior', color='b', alpha=0.25)
+    if legend:
+        plt.legend();
+    ax = plt.gca()
+    ax.set_xlabel("Parameter")
+    ax.set_yticks([])
+
+
+    if savefigure:
+        plt.savefig('probs.pdf')
+    plt.show()
