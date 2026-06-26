63a227105d7e094b07cb459eb9389eb24c3a6a90
jwhite <jtwhite1000@gmail.com>
2026-06-26 12:08:46 -0600

    added pls and gpr example notebooks

--- tutorials/part0_01_intro_to_bayes/bayes_helper.py
+++ tutorials/part0_01_intro_to_bayes/bayes_helper.py
@@ -31,10 +31,10 @@ def plot_posterior(prior_mean, prior_std, likeli_mean, likeli_std, legend=True, 
     post_mean, post_std = gaussian_multiply(prior_mean, prior_std, likeli_mean, likeli_std)
 
     xs, ys = pyemu.plot.plot_utils.gaussian_distribution(prior_mean, prior_std)
-    plt.plot(xs, ys, color='k', ls='--', lw=3.0, label='prior')
+    plt.plot(xs, ys, color='0.5', ls='--', lw=3.0, label='prior')
 
     xs, ys = pyemu.plot.plot_utils.gaussian_distribution(likeli_mean, likeli_std)
-    plt.plot(xs, ys, color='g', ls='--', lw=3.0, label='likelihood')
+    plt.plot(xs, ys, color='r', ls='--', lw=3.0, label='likelihood')
 
     xs, ys = pyemu.plot.plot_utils.gaussian_distribution(post_mean, post_std)
     plt.fill_between(xs, 0, ys, label='posterior', color='b', alpha=0.25)
